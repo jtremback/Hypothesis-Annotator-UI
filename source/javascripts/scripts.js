b4e9b28c99b6cde0cd8b14309c6a9a2e49f946af
Jehan Tremback <jehan.tremback@gmail.com>
2012-05-02 00:31:03 -0700

    More layout stuff, plus some JS. Still need to fix some jquery things, specifically changing classes and having them update in css.

--- source/javascripts/scripts.js
+++ source/javascripts/scripts.js
@@ -1,6 +1,20 @@
 $(document).ready(function() {
-  $("#controls1").hide();
-  $("#meta1").hover(function() {
-    $("#controls1").slideToggle();
+  $(".a-detail").hide();
+  $(".a-sidepane").hide();
+
+  $(".a-heatmap_tab").click(function() {
+    $(this).parents(".#source").css("margin-right", "510px")
+    $(".a-sidepane").show();
+  });
+
+  $(".a-threadexp").click(function() {
+    $(this).parents(".a-widget:eq(0)").children(".a-body").children(".a-content").slideToggle("fast");
+    $(this).parents(".a-widget:eq(0)").children(".a-replies:eq(0)").slideToggle("fast");
+    $(this).toggleClass(".a-closed");
+  });
+
+  $("#a-bo1024").click(function() {
+    $(".a-bucket").hide();
+    $(".a-detail").show();
   });
 });
