26145426dad76b943b4ee401f9c7b47c64e08c0f
Jehan Tremback <jehan.tremback@gmail.com>
2012-05-04 16:43:10 -0700

    got controls working, and collapse in rudimentary state. JS IS SET UP FOR ONLY DETAIL VIEW RIGHT NOW. Will change back on next commit.

--- source/javascripts/scripts.js
+++ source/javascripts/scripts.js
@@ -1,20 +1,46 @@
 $(document).ready(function() {
-  $(".a-detail").hide();
-  $(".a-sidepane").hide();
+//  $(".a-detail").hide();
+//  $(".a-sidepane").hide();
+  $(".a-fakecontrols").hide();
 
+//VIEW SWITCHER
   $(".a-heatmap_tab").click(function() {
-    $(this).parents(".#source").css("margin-right", "510px")
+    $(this).parents(".#source").css("margin-right", "510px");
     $(".a-sidepane").show();
   });
 
+
+//COMMENT COLLAPSER
   $(".a-threadexp").click(function() {
+
+//Traverses to content and toggles it
     $(this).parents(".a-widget:eq(0)").children(".a-body").children(".a-content").slideToggle("fast");
-    $(this).parents(".a-widget:eq(0)").children(".a-replies:eq(0)").slideToggle("fast");
-    $(this).toggleClass("a-closed");
+
+//Traverses to replies listing and toggles it
+    $(this).parents(".a-.widget:eq(0)").children(".a-replies:eq(0)").slideToggle("fast");
+
+//Switches plus/minus
+    $(this).parents(".a-widget:eq(0)").toggleClass(".a-closed");
   });
 
-  $("#a-bo1024").click(function() {
+
+//CONTROL SHOWER
+//
+  $(".a-body").hover(function() {
+//Shows controls
+    $(this).siblings(".a-meta").children(".a-fakecontrols").fadeToggle();
+  });
+
+//HOVER HILIGHTS
+  $(".a-summary").hover(function() {
+    var hash = $(this).attr("id");
+    alert(hash);
+  });
+
+//BUCKET VIEW TOGGLE
+  $("#a-xb9").click(function() {
     $(".a-bucket").hide();
     $(".a-detail").show();
   });
+
 });
