92db228b672420c2a0eaea63b77fc6993a6c1586
Jehan Tremback <jehan.tremback@gmail.com>
2012-05-05 12:26:08 -0700

    Fixed controls and made input box. Still need to switch between views, overhaul padding and do highlighting etc.

--- source/javascripts/scripts.js
+++ source/javascripts/scripts.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 //  $(".a-detail").hide();
 //  $(".a-sidepane").hide();
   $(".a-fakecontrols").hide();
+  $(".a-writer").hide();
 
 //VIEW SWITCHER
   $(".a-heatmap_tab").click(function() {
@@ -14,21 +15,31 @@ $(document).ready(function() {
   $(".a-threadexp").click(function() {
 
 //Traverses to content and toggles it
-    $(this).parents(".a-widget:eq(0)").children(".a-body").children(".a-content").slideToggle("fast");
+    $(this).parents(".a-widget:eq(0)").find(".a-content").slideToggle("fast");
 
 //Traverses to replies listing and toggles it
-    $(this).parents(".a-.widget:eq(0)").children(".a-replies:eq(0)").slideToggle("fast");
+    $(this).parents(".a-widget:eq(0)").children(".a-replies:eq(0)").slideToggle("fast");
 
 //Switches plus/minus
-    $(this).parents(".a-widget:eq(0)").toggleClass(".a-closed");
+    $(this).toggleClass("a-closed");
   });
 
 
-//CONTROL SHOWER
-//
-  $(".a-body").hover(function() {
+//CONTROL DISPLAY
+  $(".a-nothread").hover(function() {
 //Shows controls
-    $(this).siblings(".a-meta").children(".a-fakecontrols").fadeToggle();
+    $(this).children(".a-meta").children(".a-fakecontrols").fadeToggle(1000);
+  });
+
+//WRITER SHOWER
+  $(".a-write").click(function() {
+//Shows writer
+    $(this).parents(".a-widget:eq(0)").find(".a-writer:eq(0)").fadeIn();
+  });
+
+  $(".a-save").addClass(".a-delete").click(function() {
+//Hides writer
+    $(this).parents(".a-writer:eq(0)").fadeOut();
   });
 
 //HOVER HILIGHTS
